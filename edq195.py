5aa2e589464c8f9c7274810ffe471b55ab33d22e
leon <334395083@qq.com>
2018-12-07 15:10:11 +0800

    11

--- edq195.py
+++ edq195.py
@@ -43,7 +43,7 @@ class AppDynamicsJob(unittest.TestCase):
         for x in lis:
             flag=AppDynamicsJob.isElementExist(self,x)
             if flag:
-                yesterday = (date.today() + timedelta(days=-2)).strftime("%Y-%m-%d")
+                yesterday = (date.today() + timedelta(days=-1)).strftime("%Y-%m-%d")
                 print(yesterday)
                 spantext=x.find_element_by_xpath('span').text
                 print(spantext)
